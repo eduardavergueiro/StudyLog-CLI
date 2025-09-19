ba9dca4d280f491d9a5aba0d3876daa449e9c3dd
Eduarda <zoospencer4@gmail.com>
2025-09-19 16:37:01 -0300

    made the menu look a little nicer

--- src/DisplayMenu.java
+++ src/DisplayMenu.java
@@ -2,11 +2,11 @@ public class DisplayMenu {
 
     public void displayMenu(){
         System.out.println("✦•────────────────────•✦•────────────────────•✦");
-        System.out.println("1. New subject");
-        System.out.println("2. New task");
-        System.out.println("3. Select subject");
-        System.out.println("4. Select task");
-        System.out.println("5. Exit");
+        System.out.println("1. New subject                                 |");
+        System.out.println("2. New task                                    |");
+        System.out.println("3. Select subject                              |");
+        System.out.println("4. Select task                                 |");
+        System.out.println("5. Exit                                        |");
         System.out.println("✦•────────────────────•✦•────────────────────•✦");
         System.out.print(">>");
     }
